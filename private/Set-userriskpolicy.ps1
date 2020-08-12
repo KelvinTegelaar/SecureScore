f1c11375507048528816c192159266ccae86aae0
Kelvin Tegelaar 💻 Lime Networks <kelvin@limenetworks.nl>
2020-08-12 21:39:33 +0200

    Fixing weird duplicate file issue

--- private/Set-userriskpolicy.ps1
+++ private/Set-userriskpolicy.ps1
@@ -4,10 +4,8 @@ function Set-UserRiskPolicy {
     Write-Warning "This policy has to be activated manually, but when using external tools such as the cyberdrain.com location monitoring script you can use this method to tell the Secure Score API you are using a third party solution. Would you like to continue?" -WarningAction Inquire
   } 
 
-  if ($script:ExternallyResolved) {
-    Set-ExternallyResolved -issue 'UserRiskPolicy'
-
-}
+  if (!$script:ExternallyResolve) {
+    Write-Warning "Marking issue $($issue) as externally resolved by third party tool." -foregroundcolor Green
     $body = @"
 {
   "assignedTo": "",
