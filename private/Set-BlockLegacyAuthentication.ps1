c204e330d64e6bf28bedd566b3f0b13cdd02b998
robkoo <robkoo@users.noreply.github.com>
2021-08-21 11:50:36 +0200

    Add files via upload

---
+++ private/Set-BlockLegacyAuthentication.ps1
@@ -0,0 +1,14 @@
+function Set-BlockLegacyAuthentication {
+    if (!$script:confirmed) {
+        Write-Warning "This will enable multi-factor authentication for all admin users, and prompt them at first logon to configure MFA. Do you want to continue?" -WarningAction Inquire  
+    } 
+    if ($script:ExternallyResolved) {
+        Set-ExternallyResolved -issue 'BlockLegacyAuthentication'
+    }
+    else {
+        $mf = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+        $mf.RelyingParty = "*"
+        $mfa = @($mf)
+        $admins = Get-MsolRoleMember -TenantId $tenant.tenantid -RoleObjectId (Get-MsolRole -RoleName "Company Administrator").ObjectId | Set-MsolUser -StrongAuthenticationRequirements $mfa   
+    }
+}
